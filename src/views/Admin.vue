2ab6f725d715a5f4688eb5b96aad4de234a24a9c
Alexandre Delafosse <alexandre.delafosse@edu.devinci.fr>
2021-03-17 19:28:15 +0100

    'FirtCommit'

---
+++ src/views/Admin.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <h1>l'admin</h1>
+</div>
+</template>
+
+<script>
+export default {
+name: "Admin"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Blog.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="main">
+  <h1>TG</h1>
+</div>
+</template>
+
+<script>
+export default {
+name: "Blog"
+}
+</script>
+
+<style scoped>
+
+</style>
